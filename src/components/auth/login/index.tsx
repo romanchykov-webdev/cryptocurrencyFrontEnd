f6e2cc0e19e77c213c2c81477c2acd0626540dac
romanchykov-webdev <romanchykov.webdev@gmail.com>
2024-05-14 10:58:59 +0200

    -Component layout login and registration

--- src/components/auth/login/index.tsx
+++ src/components/auth/login/index.tsx
@@ -1,15 +1,32 @@
 import React from 'react';
-
+import {Button, TextField, Typography} from "@mui/material";
 const LoginPage = () => {
-
-
   return (
-    <div>
+    <>
+        <Typography variant="h2"  fontFamily='Poppins' textAlign='center'>
+            Авторизация
+        </Typography>
+        <Typography variant="body1" marginBottom={3}  fontFamily='Poppins' textAlign='center'>
+            Введите ваш login и Password
+        </Typography>
 
-      <h1 className=''>Login page</h1>
-
-
-    </div>
+        <TextField fullWidth={true} margin='normal' label="Email" variant="outlined" placeholder='Введите ваш Email'/>
+        <TextField fullWidth={true} type='password' margin='normal' label="Password" variant="outlined" placeholder='Введите ваш Password'/>
+        <Button sx={{
+            fontFamily:'Poppins',
+            marginTop:2,
+            width:'60%',
+            marginBottom:2
+        }} variant="contained">Войти</Button>
+        <Typography variant="body1"  fontFamily='Poppins'
+        sx={{
+            fontFamily:'Poppins'
+        }}
+        >
+            У вас нет аккаунт?
+            <span className='incitingText'>Регистрация</span>
+        </Typography>
+    </>
   );
 };
 
--- src/components/auth/register/index.tsx
+++ src/components/auth/register/index.tsx
@@ -1,10 +1,37 @@
 import React from 'react';
+import {Button, TextField, Typography} from "@mui/material";
 
 const RegisterPage = () => {
   return (
-    <div>
-      <h1>registration page</h1>
-    </div>
+      <>
+          <Typography variant="h2"  fontFamily='Poppins' textAlign='center'>
+              Регистрация
+          </Typography>
+          <Typography variant="body1" marginBottom={3}  fontFamily='Poppins' textAlign='center'>
+              Введите данние для регистрации
+          </Typography>
+
+          <TextField fullWidth={true} margin='normal' label="Имя" variant="outlined" placeholder='Введите ваше имя'/>
+          <TextField fullWidth={true} margin='normal' label="Username" variant="outlined" placeholder='Введите ваш username'/>
+          <TextField fullWidth={true} margin='normal' label="Email" variant="outlined" placeholder='Введите ваш Email'/>
+          <TextField fullWidth={true} type='password' margin='normal' label="Password" variant="outlined" placeholder='Введите ваш Password'/>
+          <TextField fullWidth={true} type='password' margin='normal' label="Password" variant="outlined" placeholder='Повторите ваш Password'/>
+
+          <Button sx={{
+              fontFamily:'Poppins',
+              marginTop:2,
+              width:'60%',
+              marginBottom:2
+          }} variant="contained">Регистрация</Button>
+          <Typography variant="body1"  fontFamily='Poppins'
+                      sx={{
+                          fontFamily:'Poppins'
+                      }}
+          >
+              У вас есть аккаунта?
+              <span className='incitingText'>Авторизация</span>
+          </Typography>
+      </>
   );
 };
 
